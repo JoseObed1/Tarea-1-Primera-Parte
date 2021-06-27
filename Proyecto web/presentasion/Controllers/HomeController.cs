cbcff2496ccdd234b903d4b726fc4e726b97f767
JoseObed1 <82057538+JoseObed1@users.noreply.github.com>
2021-06-26 22:26:27 -0400

    Beta 0.3 - Actualizacion de framework
    Se actualizo de net core a net framework

---
+++ Proyecto web/presentasion/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Extensions.Logging;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication3.Controllers
+{
+    public class HomeController : Controller
+    {
+        //private readonly ILogger<HomeController> _logger;
+
+        /*public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }*/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult sobrenosotros()
+        {
+            return View();
+        }
+
+        public ActionResult nuevoPost()
+        {
+            return View();
+        }
+
+        public ActionResult LoginRegister()
+        {
+            return View();
+        }
+
+
+        public ActionResult Privacy()
+        {
+            return View();
+        }
+
+        public ActionResult Contactos()
+        {
+            return View();
+        }
+    }
+}
