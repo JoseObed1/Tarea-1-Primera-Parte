3772e2d9454f21fd0da28557742c494205e3d495
Caterina2001 <84465451+Caterina2001@users.noreply.github.com>
2021-06-30 18:57:10 -0400

    beta 3.1.1

--- Proyecto web/presentasion/Controllers/HomeController.cs
+++ Proyecto web/presentasion/Controllers/HomeController.cs
@@ -34,35 +34,72 @@ namespace WebApplication3.Controllers
             return View();
         }        
         
-        [HttpPost]
-        public ActionResult LoginRegister(Cuentas datos)
-        {
-            var existe = db.Cuentas.Where(y => y.Correo.Equals(datos.Correo) && y.Password.Equals(datos.Password)).FirstOrDefault();
+        //[HttpPost]
+        //public ActionResult LoginRegister(Cuentas datos)
+        //{
+        //    var existe = db.Cuentas.Where(y => y.Correo.Equals(datos.Correo) && y.Password.Equals(datos.Password)).FirstOrDefault();
 
-            string TempoMail = datos.Correo;
-            string TempoPass = datos.Password;
+        //    string TempoMail = datos.Correo;
+        //    string TempoPass = datos.Password;
+
+        //    if (existe != null)
+        //    {
+        //        return RedirectToAction("Index", "Home");
+        //    }
+
+        //    else if (datos.Correo == "a" && datos.Password == "a")
+        //    {
+        //        return RedirectToAction("Index", "Home");
+        //    }
+
+        //    else if (testeo.ValidarLogin(datos.Correo, datos.Correo) == true)
+        //    {
+        //        RedirectToAction("Index", "Home");
+        //    }
+
+        //    else
+        //    {
+        //        return RedirectToAction("sobrenosotros", "Home");
+        //    }
+
+        //    return View();
+        //}
+
+
+        [HttpPost]
+        public ActionResult Registro(Cuentas dato)
+        {
+            if(db.Cuentas.Any(x=> x.Correo == dato.Correo))
+            {
+                ViewBag.Notification = "Ya existe esta cuenta";
+                return View();
+            }
+            else
+            {                
+                //testeo.NuevoUsuario(dato); esperar a Obed para que funcione esta parte
+                return RedirectToAction("Index", "Home");
+            }
+
+        }
+
+        [HttpPost]
+        public ActionResult InicioSesion(Cuentas datoss)
+        {
+            var existe = db.Cuentas.Where(y => y.Correo.Equals(datoss.Correo) && y.Password.Equals(datoss.Password)).FirstOrDefault();
+
+            string TempoMail = datoss.Correo;
+            string TempoPass = datoss.Password;
 
             if (existe != null)
             {
                 return RedirectToAction("Index", "Home");
             }
-
-            else if (datos.Correo == "a" && datos.Password == "a")
-            {
-                return RedirectToAction("Index", "Home");
-            }
-
-            else if (testeo.ValidarLogin(datos.Correo, datos.Correo) == true)
-            {
-                RedirectToAction("Index", "Home");
-            }
-
             else
             {
-                return RedirectToAction("sobrenosotros", "Home");
+                ViewBag.Notification = "Email o contraseña incorrecta";
+                return View();
             }
 
-            return View();
         }
 
         public ActionResult Privacy()
